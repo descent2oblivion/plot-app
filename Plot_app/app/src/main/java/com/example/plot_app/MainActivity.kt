2fa4279e15886f1909314b46d08a0746ca455a00
descent2oblivion <58195806+descent2oblivion@users.noreply.github.com>
2020-06-27 00:32:59 -0700

    Add Plot-app

---
+++ Plot_app/app/src/main/java/com/example/plot_app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.plot_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    var gr : grafica = grafica(0 ,0)
+
+    fun disp(view : View){
+        gr = grafica(txt_cata.getText().toString().toInt(), txt_catb.getText().toString().toInt())
+
+        wevi.loadData(gr.ht, "text/html", "UTF-8")
+    }
+}
